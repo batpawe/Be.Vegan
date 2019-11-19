e9e68f91d9fab4342259866b25a8b3b0ef7e0de9
plmklimek <plmateuszklimek@gmail.com>
2019-11-19 11:02:01 +0100

    Added web project for test Django API

---
+++ wegemania-web/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import TextBlock from "./components/TextBlock";
+const App = () => {
+  return <TextBlock />;
+};
+
+export default App;
---
+++ wegemania-web/src/style/TextBlockStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  font-size: 40px;
+  text-align: center;
+`;
+export const TextEffect = styled.p`
+  color: red;
+  display: inline;
+`;
