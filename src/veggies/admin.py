51b1f56d906ab5d10366fd4588b881acb359bec1
ChePsik <batpawe@gmail.com>
2019-12-15 19:34:45 +0100

    Merging Chepsik's and AdamGinna's commit. Working Django with two databases. MYSQL is 100% done, Mongo has to be configured.

--- src/veggies/admin.py
+++ src/veggies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(Rating_Recipe)
+admin.site.register(Ingredient_List)
+admin.site.register(Preference)
+admin.site.register(Food_To_Substitute)
+admin.site.register(Food_Substitute)
+admin.site.register(Restaurant)
+admin.site.register(Rating_Restaurant)
+admin.site.register(Report_Res)
+
+
 
 # Register your models here.
--- src/veggies/models.py
+++ src/veggies/models.py
@@ -1,71 +1,71 @@
 from django.db import models
 
 
-class Users(models.Model):
-    login = models.CharField("login", unique=True, blank=True)
-    email = models.CharField("email", unique=True, blank=True)
+class User(models.Model):
+    login = models.CharField("login", unique=True, blank=True, max_length = 120)
+    email = models.CharField("email", unique=True, blank=True, max_length = 120)
     created_at = models.DateField("created_at")
     height = models.PositiveIntegerField("height", null=True, blank=True)
-    weight = models.DecimalField("weight", null=True, blank=True)
+    weight = models.DecimalField("weight", null=True, blank=True, max_digits = 4, decimal_places = 2)
     age = models.PositiveIntegerField("age", null=True, blank=True)
-    activity = models.PositiveIntegerField("activity", max_length=4,null=True, blank=True)
+    activity = models.PositiveIntegerField("activity", null=True, blank=True)
 
 
-class Ingredients(models.Model):
-    name = models.CharField("name", unique=True)
+class Ingredient(models.Model):
+    name = models.CharField("name", unique=True, max_length = 120)
     calories = models.PositiveIntegerField("calories", null=True, blank=True)
 
 
-class Recipes(models.Model):
+class Recipe(models.Model):
     recipe_decryption = models.TextField("recipe_descyption")
     recipe_foto = models.ImageField("recipe_foto")
-    id_user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    id_user = models.ForeignKey(User, on_delete = models.CASCADE)
 
 
-class Rating_Recipes(models.Model):
-    id_recipe = models.ForeignKey(Recipes)
-    id_user = models.ForeignKey(Users)
+class Rating_Recipe(models.Model):
+    id_recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+    id_user = models.ForeignKey(User, on_delete = models.CASCADE)
     user_comment = models.TextField("user_comment", null=True, blank=True)
-    rating = models.PositiveSmallIntegerField(max_length=5)
+    rating = models.PositiveSmallIntegerField()
 
 class Ingredient_List(models.Model):
-    id_ingredient = models.ForeignKey(Ingredients)
-    id_recipes = models.ForeignKey(Recipes)
+    id_ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
+    id_recipes = models.ForeignKey(Recipe, on_delete = models.CASCADE)
 
-class Preferences(models.Model):
-    id_user = models.ForeignKey(Users)
-    id_ingredients = models.ForeignKey(Ingredients)
+class Preference(models.Model):
+    id_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_ingredients = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
     like = models.IntegerField('like')
 
-class Food_To_Substitutes(models.Model):
-    food_name = models.CharField("food_name", unique=True)
-    description = models.TextField("decryption", null=True, blank=True)
+class Food_To_Substitute(models.Model):
+    food_name = models.CharField("food_name", unique=True, max_length = 120)
+    description = models.TextField("decription", null=True, blank=True)
 
-class Food_Substitutes(models.Model):
-    id_vegan = models.ForeignKey(Ingredients)
-    id_food_to_substitute = models.ForeignKey(Food_To_Substitutes)
+class Food_Substitute(models.Model):
+    id_vegan = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
+    id_food_to_substitute = models.ForeignKey(Food_To_Substitute, on_delete = models.CASCADE)
 
-class Restaurants(models.Model):
-    id_moderator = models.ForeignKey(Users)
-    name = models.CharField("name",unique=True)
-    city = models.CharField("city")
+class Restaurant(models.Model):
+    id_moderator = models.ForeignKey(User, on_delete = models.CASCADE)
+    name = models.CharField("name",unique=True, max_length = 120)
+    city = models.CharField("city", max_length = 120)
     foto = models.ImageField("foto",null=True, blank=True)
-    street = models.CharField("street")
+    street = models.CharField("street", max_length = 120)
     street_number = models.PositiveIntegerField("street_number")
-    latX = models.DecimalField("latX")
-    longY = models.DecimalField("LongY")
+    latX = models.DecimalField("latX", max_digits = 12, decimal_places = 10)
+    longY = models.DecimalField("LongY", max_digits = 12, decimal_places = 10)
     hours = models.TextField("hours",null=True, blank=True)
-    rating = models.DecimalField("rating")
+    rating = models.DecimalField("rating", max_digits = 4, decimal_places = 2)
     description = models.TextField("description")
 
-class Rating_Restaruant(models.Model):
-    id_user = models.ForeignKey(Users)
-    id_restaurant = models.ForeignKey(Restaurants)
+class Rating_Restaurant(models.Model):
+    id_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
     comment = models.TextField("commet", null=True, blank=True)
-    rating = models.DecimalField("rating")
+    rating = models.DecimalField("rating", max_digits = 4, decimal_places = 2)
 
 
 class Report_Res(models.Model):
-    id_restaurant = models.ForeignKey(Restaurants)
-    id_user = models.ForeignKey(Users)
+    id_restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
+    id_user = models.ForeignKey(User, on_delete = models.CASCADE)
     description = models.TextField("description")
