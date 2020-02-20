946bd1c5d72639e01820ff576ac93aad8c027a9a
ChePsik <batpawe@gmail.com>
2020-02-20 13:27:16 +0100

    restore changes

--- src/be_vegan/config.py
+++
@@ -1,8 +0,0 @@
-class SecretVariables:
-    database_password = 'M0uzvR9AoZ'
-    database_name = 'acxEWTLu6c'
-    database_user = 'acxEWTLu6c'
-    database_posts_name = 'bexw8am2pix0hsa'
-    database_posts_user = 'uvpgmh6zy50ovxs6vxwa'
-    database_posts_password = 'BRYihNi6Yu6OjQua3gJn'
-    secret_key = 'j&&7yaef2s85&afuk)3^to)da23uqflh%3g2$imp(89*@hgs6j'
