70cc238864d71fab8d2df5402a79877d03e8f3be
ChePsik <batpawe@gmail.com>
2020-03-06 09:23:09 +0100

    Heroku test with post API etc.

--- src/veggies/admin.py
+++ src/veggies/admin.py
@@ -20,12 +20,12 @@ class MultiDBModelAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         # Tell Django to populate ForeignKey widgets using a query
         # on the 'other' database.
-        return super().formfield_for_foreignkey(db_field, request, using=self.using, **kwargs)
+        return super().formfield_for_foreignkey(db_field, request, using='default', **kwargs)
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         # Tell Django to populate ManyToMany widgets using a query
         # on the 'other' database.
-        return super().formfield_for_manytomany(db_field, request, using=self.using, **kwargs)
+        return super().formfield_for_manytomany(db_field, request, using='default', **kwargs)
 
 admin.site.register(User)
 admin.site.register(Ingredient)
