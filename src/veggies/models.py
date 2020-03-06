00c0362292b6e5ea74e5b76174699b07f4b4ca3a
ChePsik <batpawe@gmail.com>
2020-03-06 13:25:48 +0100

    Heroku test with post API etc.

--- src/veggies/models.py
+++ src/veggies/models.py
@@ -98,8 +98,6 @@ class Main_Post(models.Model):
     description = models.TextField("description")
     foto = models.ImageField("foto", null=True, blank=True)
     data_stamp = models.DateTimeField(default = now, blank=True)
-    class Meta:
-        app_label = 'post'
 
 class Reply_Post(models.Model):
     description = models.TextField("description")
@@ -107,8 +105,6 @@ class Reply_Post(models.Model):
     foto = models.ImageField("foto", null=True, blank=True)
     data_stamp = models.DateField(default = now)
     id_post = models.ForeignKey(Main_Post, on_delete=models.CASCADE)
-    class Meta:
-        app_label = 'post'
 
 class Rating_Restaurant(models.Model):
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
