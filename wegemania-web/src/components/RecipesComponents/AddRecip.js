bd4af4cd58f4b0bc7185e162972156e87c2312a6
Mateusz Klimek <plmateuszklimek@gmail.com>
2020-04-07 13:30:20 +0200

    7 kwietnia 2020 13:30

--- wegemania-web/src/components/RecipesComponents/AddRecip.js
+++ wegemania-web/src/components/RecipesComponents/AddRecip.js
@@ -20,7 +20,7 @@ import {
   TextAreaMethod,
   MethodContainer,
   ProductsContainer,
-  HighlightText
+  HighlightText,
 } from "../../styles/AddForms";
 import UploadImage from "../../images/upload.png";
 import "../../App.css";
@@ -37,12 +37,26 @@ const AddRecipt = () => {
   const [methods, setMethods] = useState("");
   const [productsArray, setProductsArray] = useState([[], [], []]);
   const [newMethod, setNewMethod] = useState("");
-  const handleMethods = e => {
+  const [description, setDescription] = useState("");
+  const [recipeName, setRecipeName] = useState("");
+  let tempProducts = [
+    {
+      name: "",
+      quanity: 0,
+    },
+  ];
+  const [products, setProducts] = useState(tempProducts);
+  const sendRequest = () => {
+    console.log(recipeName);
+    console.log(description);
+    console.log(products);
+  };
+  const handleMethods = (e) => {
     let temp = methods;
     temp = e.target.value;
     setMethods(temp);
   };
-  const handleProducts = e => {
+  const handleProducts = (e) => {
     let temp = productsArray;
     for (var i = 0; i < numberProducts; i++) {
       if (temp[i] === undefined) {
@@ -52,13 +66,14 @@ const AddRecipt = () => {
     temp[e.target.id][0] = e.target.value;
     setProductsArray(temp);
   };
-  const handleQuanity = e => {
+  const handleQuanity = (e) => {
     let temp = productsArray;
     for (var i = 0; i < numberProducts; i++) {
       if (temp[i] === undefined) {
         temp[i][0] = "";
       }
     }
+    console.log(productsArray);
     temp[e.target.id][1] = e.target.value;
     setProductsArray(temp);
   };
@@ -76,24 +91,28 @@ const AddRecipt = () => {
       temp.push(
         <ProductsContainer style={{ width: "100%" }}>
           <TextColumnInput
+            style={{ width: "300px" }}
             type="text"
             placeholder="Nazwa produktu"
             id={i}
+            key={i}
             value={productsArray[i][0]}
-            onChange={e => {
+            onChange={(e) => {
               handleProducts(e);
             }}
           />
           <TextColumnInput
+            style={{ width: "200px" }}
             type="number"
             id={i}
+            key={i}
             placeholder="Wprowadź ilość"
             value={productsArray[i][1]}
-            onChange={e => {
+            onChange={(e) => {
               handleQuanity(e);
             }}
           />
-          <HighlightText>gram</HighlightText>
+          <HighlightText style={{ width: "100px" }}>gram</HighlightText>
           <img
             style={{ width: 25 }}
             src={CloseImage}
@@ -104,7 +123,7 @@ const AddRecipt = () => {
     }
     return (
       <div style={{ display: "flex", "justify-content": "space-between" }}>
-        <ReciptLabel for="products">
+        <ReciptLabel for="products" style={{ width: "100%" }}>
           <p style={{ "font-size": "20px", "font-weight": "bold" }}>
             Produkty:
           </p>
@@ -114,51 +133,17 @@ const AddRecipt = () => {
       </div>
     );
   };
-  const MethodFields = () => {
-    return (
-      <div style={{ display: "flex", "justify-content": "center" }}>
-        <ReciptLabel
-          for="products"
-          style={{ width: "100%", justifyContent: "center" }}
-        >
-          <div style={{ display: "flex", "flex-direction": "column" }}>
-            <p style={{ "font-size": "20px", "font-weight": "bold" }}>
-              Sposób przygotowania:
-            </p>
-            <MethodContainer
-              style={{ width: "40%", margin: "1% auto 1% auto" }}
-            >
-              <TextAreaMethod
-                style={{ width: "100%", height: "200px" }}
-                type="text"
-                id={1}
-                value={newMethod}
-                onChange={e => {
-                  setNewMethod(e.target.value);
-                }}
-              />
-            </MethodContainer>
-          </div>
-        </ReciptLabel>
-      </div>
-    );
-  };
+
   const nextProductField = () => {
     let temp = numberProducts + 1;
     setNumberProducts(temp);
   };
-  const nextMethodField = () => {
-    let temp = numberMethods + 1;
-    setNumberMethods(temp);
-  };
+
   const prevProductField = () => {
     let temp = numberProducts - 1;
     setNumberProducts(temp);
   };
-  const prevMethodField = () => {
-    let temp = numberMethods - 1;
-    setNumberMethods(temp);
-  };
+
   return (
     <Container>
       <div
@@ -166,17 +151,108 @@ const AddRecipt = () => {
           display: "flex",
           "justify-content": "center",
           "flex-direction": "column",
-          "align-items": "center"
+          "align-items": "center",
         }}
       >
         <InputLabel for="name" style={{ "font-size": "20px" }}>
           Nazwa:
         </InputLabel>
-        <TextInput type="text" id="name" placeholder="Wprowadź nazwę dania" />
+        <TextInput
+          value={recipeName}
+          onChange={(e) => {
+            setRecipeName(e.target.value);
+          }}
+          type="text"
+          id="name"
+          placeholder="Wprowadź nazwę dania"
+        />
       </div>
       <ColumnContainer style={{ display: "flex", "flex-direction": "column" }}>
-        <MethodFields />
-        <ProductFields />
+        <div style={{ display: "flex", "justify-content": "center" }}>
+          <ReciptLabel
+            for="products"
+            style={{ width: "100%", justifyContent: "center" }}
+          >
+            <div style={{ display: "flex", "flex-direction": "column" }}>
+              <p style={{ "font-size": "20px", "font-weight": "bold" }}>
+                Sposób przygotowania:
+              </p>
+              <MethodContainer
+                style={{ width: "40%", margin: "1% auto 1% auto" }}
+              >
+                <TextAreaMethod
+                  placeholder="Wprowadź sposób przygotowania"
+                  style={{ width: "100%", height: "200px" }}
+                  type="text"
+                  value={description}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
+                />
+              </MethodContainer>
+            </div>
+          </ReciptLabel>
+        </div>
+        <p
+          style={{
+            "font-size": "20px",
+            "font-weight": "bold",
+            "text-align": "center",
+          }}
+        >
+          Produkty:
+        </p>
+        {products &&
+          products.map((product, index) => {
+            return (
+              <div>
+                <ProductsContainer style={{ display: "flex" }}>
+                  <TextColumnInput
+                    style={{ width: "300px" }}
+                    placeholder="Nazwa produktu"
+                    type="text"
+                    value={product.name}
+                    onChange={(e) => {
+                      let temp = products;
+                      temp[index].name = e.target.value;
+                      setProducts([...temp]);
+                    }}
+                  />
+                  <TextColumnInput
+                    style={{ width: "200px" }}
+                    placeholder="Wprowadź ilość"
+                    type="text"
+                    value={products.quanity}
+                    onChange={(e) => {
+                      let temp = products;
+                      temp[index].quanity = e.target.value;
+                      setProducts([...temp]);
+                    }}
+                  />
+                  <HighlightText style={{ width: "100px" }}>gram</HighlightText>
+                  <img
+                    style={{ width: 25 }}
+                    src={CloseImage}
+                    onClick={() => {
+                      let temp = products;
+                      temp.splice(index, 1);
+                      setProducts([...temp]);
+                    }}
+                  />
+                </ProductsContainer>
+              </div>
+            );
+          })}
+        <AddItem
+          onClick={() => {
+            let tempArray = products;
+            let temp = { name: "", number: 0 };
+            tempArray.push(temp);
+            setProducts([...tempArray]);
+          }}
+        >
+          Dodaj produkt
+        </AddItem>
       </ColumnContainer>
       <ImagesContainer>
         <div className="image-upload">
@@ -186,11 +262,17 @@ const AddRecipt = () => {
           <input
             id="file-input-0"
             type="file"
-            onChange={e => handleChange(0, e)}
+            onChange={(e) => handleChange(0, e)}
           />
         </div>
       </ImagesContainer>
-      <Button>Dodaj przepis</Button>
+      <Button
+        onClick={() => {
+          sendRequest();
+        }}
+      >
+        Dodaj przepis
+      </Button>
     </Container>
   );
 };
