f07173eac940fbc5cb0581cbeb6a1fe0c6eb5a7a
ChePsik <batpawe@gmail.com>
2020-04-22 12:13:16 +0200

    AWS Fix

--- src/be_vegan/settings.py
+++ src/be_vegan/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .config import SecretVariables
+from .config0 import SecretVariables
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSFR_TRUSTED_ORIGINS = ["*"]
+
+AWS_ACCESS_KEY_ID = 'AKIA37SVVXBHZZZ2PQ2T'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_URL = 'https://cloud-cube-eu.s3.amazonaws.com/kgohyhjgpt5s'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_STORAGE_BUCKET_NAME = 'cloud-cube-eu'
+AWS_DEFAULT_ACL = 'public-read'
 
 # Application definition
 
